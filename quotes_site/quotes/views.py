c19f3c24cbfefeeb08996d9230c89097bcbe55c5
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2025-08-07 16:35:12 +0200

    start

---
+++ quotes_site/quotes/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegisterForm
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('main')
+    else:
+        form = RegisterForm()
+    return render(request, 'quotes/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('main')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'quotes/login.html', {'form': form})
